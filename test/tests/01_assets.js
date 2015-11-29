345328eabf4721c6295e414cd909f1dce5cdeabe
Rey Dhuny <rey@me.com>
2015-11-29 18:30:14 +0000

    Update `01_assets.js`
    Instead of checking an array of hardcoded assets have loaded, check what has
    been linked to in the HTML page has loaded.
    
    This is better because its checking that the link in the HTML is correct, and
    that the actual assets have loaded. I think :)

--- test/tests/01_assets.js
+++ test/tests/01_assets.js
@@ -5,43 +5,17 @@ casper.test.begin("Testing 01_assets", function suite(test) {
     test.assertUrlMatch("/", "URL is root");
   });
 
-  var cssFiles = [
-    "assets/css/style.css",
-    "https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.css",
-    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css"
-  ];
   casper.then(function() {
-    test.assertResourceExists("assets/images/jumbo_home.jpg"); 
-  });
-
-  casper.then(function() {
+    var cssFiles = this.getElementsAttribute('link[type="text/css"]', 'href');
     cssFiles.forEach(function(css) {
       test.assertResourceExists(css, css + " was loaded"); 
     });
   });
 
-  var jsFiles = [
-    "assets/javascripts/main.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js",
-    "https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js",
-    "//cdn.rawgit.com/namuol/cheet.js/master/cheet.min.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/js/bootstrap.min.js"
-  ];
-
   casper.then(function() {
+    var jsFiles = this.getElementsAttribute('script', 'src');
     jsFiles.forEach(function(js) {
       test.assertResourceExists(js, js + " was loaded"); 
-    });
-  });
-
-  var imageFiles = [
-    // no idea why it won't load favicon
-    // "assets/images/favicon.png",
-  ]
-
-  casper.then(function() {
-    imageFiles.forEach(function(image) {
-      test.assertResourceExists(image, image + " was loaded"); 
     });
   });
 
